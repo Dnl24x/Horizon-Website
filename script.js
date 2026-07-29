910bc473e6ae22713d4dbb12d803086137819067
Dnl24x <dnlchirosca25@gmail.com>
2026-07-29 12:43:32 +0100

    Fixed More Bugs

--- script.js
+++ script.js
@@ -1,50 +1,94 @@
-function createAccount() {
+function createAccount(){
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
 
-    if(username == "" || password == "") {
-        document.getElementById("message").innerHTML = "Please fill everything!";
-        return;
-    }
 
-    localStorage.setItem("horizonUsername", username);
-    localStorage.setItem("horizonPassword", password);
+if(username=="" || password==""){
 
-    document.getElementById("message").innerHTML = 
-    "Account created! You can now sign in.";
+document.getElementById("message").innerHTML =
+"Please fill everything!";
+
+return;
 
 }
 
-function login() {
 
-    let username = document.getElementById("loginUsername").value;
-    let password = document.getElementById("loginPassword").value;
+localStorage.setItem("horizonUsername", username);
+localStorage.setItem("horizonPassword", password);
 
-    let savedUsername = localStorage.getItem("horizonUsername");
-    let savedPassword = localStorage.getItem("horizonPassword");
 
-    if(username == savedUsername && password == savedPassword) {
-
-        localStorage.setItem("horizonLoggedIn", "true");
-
-        window.location.href = "account.html";
-
-    } else {
-
-        document.getElementById("message").innerHTML =
-        "Incorrect username or password.";
-
-    }
-
-    function logout() {
-
-    localStorage.removeItem("horizonLoggedIn");
-    localStorage.removeItem("horizonUsername");
-    localStorage.removeItem("horizonPassword");
-
-    window.location.href = "index.html";
+document.getElementById("message").innerHTML =
+"Account created! You can now sign in.";
 
 }
+
+
+
+
+
+function login(){
+
+
+let username =
+document.getElementById("loginUsername").value;
+
+
+let password =
+document.getElementById("loginPassword").value;
+
+
+
+let savedUsername =
+localStorage.getItem("horizonUsername");
+
+
+let savedPassword =
+localStorage.getItem("horizonPassword");
+
+
+
+if(username===savedUsername && password===savedPassword){
+
+
+localStorage.setItem(
+"horizonLoggedIn",
+"true"
+);
+
+
+window.location.href="account.html";
+
+
+}
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"Incorrect username or password.";
+
+
+}
+
+
+
+}
+
+
+
+
+
+function logout(){
+
+
+localStorage.removeItem("horizonLoggedIn");
+localStorage.removeItem("horizonUsername");
+localStorage.removeItem("horizonPassword");
+localStorage.removeItem("horizonRank");
+
+
+window.location.href="index.html";
+
 
 }
