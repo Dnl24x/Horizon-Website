52e1e6ca82cb52ecd24c5061d68645473d4203c3
Dnl24x <dnlchirosca25@gmail.com>
2026-07-30 12:38:50 +0100

    Fixed Bugs

---
+++ Horizon-Website-main/js/script.js
@@ -0,0 +1,195 @@
+// ==========================
+// Wippix Account System
+// ==========================
+
+
+// Create Account
+
+function createAccount(){
+
+
+let username =
+document.getElementById("username").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+if(username === "" || password === ""){
+
+
+document.getElementById("message").innerHTML =
+"Please enter username and password.";
+
+
+return;
+
+
+}
+
+
+
+
+localStorage.setItem(
+"wippixUsername",
+username
+);
+
+
+
+localStorage.setItem(
+"wippixPassword",
+password
+);
+
+
+
+
+// Rank System
+
+if(username === "Dnl24x"){
+
+
+localStorage.setItem(
+"wippixRank",
+"OWNER"
+);
+
+
+}
+
+else{
+
+
+localStorage.setItem(
+"wippixRank",
+"PLAYER"
+);
+
+
+}
+
+
+
+
+localStorage.setItem(
+"wippixLoggedIn",
+"true"
+);
+
+
+
+window.location.href =
+"index.html";
+
+
+}
+
+
+
+
+
+
+
+// Login
+
+
+function login(){
+
+
+let username =
+document.getElementById("loginUsername").value;
+
+
+let password =
+document.getElementById("loginPassword").value;
+
+
+
+let savedUsername =
+localStorage.getItem("wippixUsername");
+
+
+let savedPassword =
+localStorage.getItem("wippixPassword");
+
+
+
+
+
+if(
+username === savedUsername &&
+password === savedPassword
+){
+
+
+localStorage.setItem(
+"wippixLoggedIn",
+"true"
+);
+
+
+
+window.location.href =
+"index.html";
+
+
+}
+
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"Incorrect username or password.";
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+// Logout
+
+
+function logout(){
+
+
+localStorage.removeItem(
+"wippixLoggedIn"
+);
+
+
+window.location.href =
+"index.html";
+
+
+}
+
+
+
+
+
+
+
+// Check login state
+
+
+function isLoggedIn(){
+
+
+return localStorage.getItem(
+"wippixLoggedIn"
+) === "true";
+
+
+}
